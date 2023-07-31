a49547b36faab48da32cf8372977916205fd4d3d
Zoom <122240395+Juanihaidar21@users.noreply.github.com>
2023-07-31 17:46:47 -0300

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1>Hola gente</h1>
+      <h1>Hola jorge</h1>
     </div>
   )
 }
