b79f7c19bd55b903a355badc8a8723d46f371ae4
jorgePz-tj1904 <jlgames1904@gmail.com>
2023-07-31 22:10:45 -0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import Home from './pages/Home/Home'
-import Detail from './pages/Detail/Detail'
-import Form from './pages/From/From';
-import {Routes, Route } from 'react-router-dom';
-import './App.css'
-
+import Home from './pages/Home/Home';
+import Detail from './pages/Detail/Detail';
+import Form from './pages/Form/From';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
 
 function App() {
-
   return (
     <div>
       <Routes>
-        <Route path='/' element={Home}/>
-        <Route path='/arma-tu-pc' element={Detail}/>
-        <Route path='/detail' element={Form}/>
+        <Route path='/' element={<Home/>} />
+        <Route path='/form' element={<Form/>} />
+        <Route path='/detail' element={<Detail/>} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter} from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom'; // Corregido el import
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render( // Usando createRoot
   <BrowserRouter>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </BrowserRouter>
-)
+);
