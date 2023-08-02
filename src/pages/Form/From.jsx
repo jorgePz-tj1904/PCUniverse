d84a7c6cd80e70f424b3ea855f93bb7e64340366
jorgePz-tj1904 <jlgames1904@gamil.com>
2023-08-02 15:56:23 -0300

    avance en el form

--- src/pages/Form/From.jsx
+++ src/pages/Form/From.jsx
@@ -1,40 +1,85 @@
-import { useState } from "react";
-import { cpus, rams, mothers, graficas, discos, fuentes} from "./componentes";
+import React, { useState } from "react";
+import { componentes } from "./componentes";
+import { useEffect } from "react";
 
-const Form=()=>{
-    const [componentsToShow , setComponentsToShow] =useState([]);
-    const [cpu, setCpu]= useState(cpus);
-    const [ram, setRam]= useState(rams);
-    const [mother, setMother] =useState(mothers);
-    const [grafica, setGrafica]= useState(graficas);
-    const [disco, setDisco]= useState(discos);
-    const [fuente, setFuente]= useState(fuentes);
+const Form = () => {
+  const [componentsToShow, setComponentsToShow] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [computer, setComputer] = useState([]);
+  const [selectedCategories, setSelectedCategories] = useState({
+    procesador: false,
+    mother: false,
+    ram: false,
+    grafica: false,
+    disco: false,
+    fuente: false,
+  });
 
+useEffect(()=>{
+  setComponentsToShow(componentes.cpus)
+},[]);
 
-    const [computer, setComputer] = useState([])
+  const componentsHandler = (event) => {
+    const componente = event.target.value.toLowerCase();
+    switch (componente) {
+      case "procesador":
+        setComponentsToShow(componentes.cpus);
+        break;
+      case "mother":
+        setComponentsToShow(componentes.mothers);
+        break;
+      case "ram":
+        setComponentsToShow(componentes.rams);
+        break;
+      case "grafica":
+        setComponentsToShow(componentes.graficas);
+        break;
+      case "disco":
+        setComponentsToShow(componentes.discos);
+        break;
+      case "fuente":
+        setComponentsToShow(componentes.fuentes);
+        break;
+      default:
+        break;
+    }
+  };
+//////////////cambiar component por component.id
+  const addToComputer = (component) => {
+    const isComponentAlreadyAdded = computer.some((comp) => comp.id === component.id);
+  if (isComponentAlreadyAdded) {
+    return alert("Ya contiene ese componente");
+  }
+  console.log(component.socket);
+    const category = component.categoria.toLowerCase();
+    if (selectedCategories[category]) {
+      return alert(`Ya seleccionaste un componente de la categoría "${category}".`);
+    }
 
-      const componentsHandler = (event) => {
-        const componente = event.target.value.toLowerCase();
-        switch (componente) {
-            case 'procesador':
-                return setComponentsToShow(cpu);
-            case 'mother':
-                return setComponentsToShow(mother);
-            case 'ram':
-                return setComponentsToShow(ram);
-            case 'grafica':
-                return setComponentsToShow(grafica);
-            case 'disco':
-                return setComponentsToShow(disco);
-            case 'fuente':
-                return setComponentsToShow(fuente);
-            default:
-                break;
-        }
+    if (component.categoria === 'mother') {
+      const compatibleProcessor = computer.find((cpu) => cpu.socket === component.socket);
+      console.log(compatibleProcessor);
+      if (!compatibleProcessor) {
+        return alert(`El procesador compatible no se encuentra para esta motherboard.`);
       }
-      const addToComputer = (component) => {
-        setComputer([...computer, component]);
-      };
+    }
+
+    setComputer([...computer, component]);
+    setTotal(total + component.precio);
+    setSelectedCategories({ ...selectedCategories, [category]: true });
+  };
+
+  const removeFromComputer = (component) => {
+    const removedComponent = computer.find((com) => com.modelo === component.modelo);
+    if (!removedComponent) return;
+
+    const updatedComputer = computer.filter((com) => com.modelo !== component.modelo);
+    setComputer(updatedComputer);
+    setTotal(total - removedComponent.precio);
+
+    const category = removedComponent.categoria.toLowerCase();
+    setSelectedCategories({ ...selectedCategories, [category]: false });
+  };
     
     return(
         <div>
@@ -49,20 +94,26 @@ const Form=()=>{
                 <button onClick={componentsHandler} value='fuente'>fuente</button>
             </div>
             <div>
-                {componentsToShow.map(component=>(<div><h1 onClick={()=>addToComputer(component)}>{component.modelo}</h1></div>))}
-                <hr />
+              {componentsToShow.map(component => (
+              <div key={component.modelo}>
+              <h1 onClick={() => addToComputer(component)}>{component.modelo}</h1>
             </div>
-            <div>
-            {computer.length > 0 && (
-               <div>
-                <h2>Componentes seleccionados:</h2>
-                {computer.map((component, index) => (
-                  <div key={index}>
-                    <h3>{component.modelo}</h3>
-                  </div>
-                ))}
+        ))}
+        <hr />
+      </div>
+      <div>
+        {computer.length > 0 && (
+          <div>
+            <h2>Componentes seleccionados:</h2>
+            {computer.map((component) => (
+              <div key={component.id}>
+                <button onClick={() => removeFromComputer(component)}>Eliminar</button>
+                <h3>{component.modelo}</h3>
               </div>
+            ))}
+          </div>
             )}
+            <h2>total: {total} $</h2> 
           </div>
         </div>
     );
--- src/pages/Form/componentes.js
+++ src/pages/Form/componentes.js
@@ -1,5 +1,7 @@
-export const cpus = [
+export const componentes = {
+  cpus: [
     {
+      id: 1,
       modelo: "Intel Core i9-11900K",
       nucleos: 8,
       hilos: 16,
@@ -7,9 +9,12 @@ export const cpus = [
       frecuenciaMaxima: "5.3 GHz",
       cache: "16 MB SmartCache",
       tdp: "125 W",
-      socket: "LGA1200"
+      socket: "LGA1200",
+      precio: 499.99,
+      categoria: 'cpu'
     },
     {
+      id: 2,
       modelo: "AMD Ryzen 9 5950X",
       nucleos: 16,
       hilos: 32,
@@ -17,9 +22,12 @@ export const cpus = [
       frecuenciaMaxima: "4.9 GHz",
       cache: "72 MB L3",
       tdp: "105 W",
-      socket: "AM4"
+      socket: "AM4",
+      precio: 699.99,
+      categoria: 'cpu'
     },
     {
+      id: 3,
       modelo: "Intel Core i7-11700K",
       nucleos: 8,
       hilos: 16,
@@ -27,12 +35,15 @@ export const cpus = [
       frecuenciaMaxima: "5.0 GHz",
       cache: "16 MB SmartCache",
       tdp: "125 W",
-      socket: "LGA1200"
+      socket: "LGA1200",
+      precio: 399.99,
+      categoria: 'cpu'
     },
-  ];
-
-export const mothers = [
+    // ... (otros componentes)
+  ],
+  mothers: [
     {
+      id: 101,
       modelo: "ASUS ROG Strix Z590-E Gaming",
       chipset: "Intel Z590",
       socket: "LGA1200",
@@ -40,8 +51,11 @@ export const mothers = [
       tipoRam: "DDR4",
       ranurasPCIe: 3,
       ranurasM2: 2,
+      precio: 299.99,
+      categoria: 'mother'
     },
     {
+      id: 102,
       modelo: "GIGABYTE B550 AORUS Pro",
       chipset: "AMD B550",
       socket: "AM4",
@@ -49,8 +63,11 @@ export const mothers = [
       tipoRam: "DDR4",
       ranurasPCIe: 2,
       ranurasM2: 2,
+      precio: 179.99,
+      categoria: 'mother'
     },
     {
+      id: 103,
       modelo: "MSI MAG B550 TOMAHAWK",
       chipset: "AMD B550",
       socket: "AM4",
@@ -58,72 +75,50 @@ export const mothers = [
       tipoRam: "DDR4",
       ranurasPCIe: 2,
       ranurasM2: 2,
+      precio: 199.99,
+      categoria: 'mother'
     },
+    // ... (otros componentes)
+  ],
+  rams: [
     {
-      modelo: "ASRock B560M Steel Legend",
-      chipset: "Intel B560",
-      socket: "LGA1200",
-      ranurasRam: 4,
-      tipoRam: "DDR4",
-      ranurasPCIe: 2,
-      ranurasM2: 1,
-    },
-    {
-      modelo: "GIGABYTE X570 AORUS Elite",
-      chipset: "AMD X570",
-      socket: "AM4",
-      ranurasRam: 4,
-      tipoRam: "DDR4",
-      ranurasPCIe: 2,
-      ranurasM2: 2,
-    },
-  ];
-
-export const rams = [
-    {
+      id: 201,
       modelo: "Corsair Vengeance LPX",
       capacidad: "16 GB",
       velocidad: "3200 MHz",
       tipo: "DDR4",
       latencia: "16-18-18-36",
-      voltaje: "1.35 V"
+      voltaje: "1.35 V",
+      precio: 89.99,
+      categoria: 'ram'
     },
     {
+      id: 202,
       modelo: "G.Skill Ripjaws V",
       capacidad: "32 GB",
       velocidad: "3600 MHz",
       tipo: "DDR4",
       latencia: "18-22-22-42",
-      voltaje: "1.35 V"
+      voltaje: "1.35 V",
+      precio: 159.99,
+      categoria: 'ram'
     },
     {
+      id: 203,
       modelo: "Crucial Ballistix",
       capacidad: "8 GB",
       velocidad: "2666 MHz",
       tipo: "DDR4",
       latencia: "16-18-18",
-      voltaje: "1.2 V"
+      voltaje: "1.2 V",
+      precio: 49.99,
+      categoria: 'ram'
     },
+    // ... (otros componentes)
+  ],
+  graficas: [
     {
-      modelo: "Kingston HyperX Fury",
-      capacidad: "64 GB",
-      velocidad: "3200 MHz",
-      tipo: "DDR4",
-      latencia: "18-22-22",
-      voltaje: "1.35 V"
-    },
-    {
-      modelo: "Team T-Force Vulcan",
-      capacidad: "16 GB",
-      velocidad: "3000 MHz",
-      tipo: "DDR4",
-      latencia: "16-18-18-38",
-      voltaje: "1.35 V"
-    }
-  ];
-
-export const graficas = [
-    {
+      id: 301,
       modelo: "NVIDIA GeForce RTX 3080",
       memoria: "10 GB GDDR6X",
       busMemoria: "320 bits",
@@ -131,8 +126,11 @@ export const graficas = [
       frecuenciaBoost: "1710 MHz",
       consumoEnergia: "320 W",
       tipoConector: "PCI Express 4.0",
+      precio: 999.99,
+      categoria: 'graficas'
     },
     {
+      id: 302,
       modelo: "AMD Radeon RX 6800 XT",
       memoria: "16 GB GDDR6",
       busMemoria: "256 bits",
@@ -140,8 +138,11 @@ export const graficas = [
       frecuenciaBoost: "2250 MHz",
       consumoEnergia: "300 W",
       tipoConector: "PCI Express 4.0",
+      precio: 799.99,
+      categoria: 'graficas'
     },
     {
+      id: 303,
       modelo: "NVIDIA GeForce RTX 3070",
       memoria: "8 GB GDDR6",
       busMemoria: "256 bits",
@@ -149,29 +150,14 @@ export const graficas = [
       frecuenciaBoost: "1725 MHz",
       consumoEnergia: "220 W",
       tipoConector: "PCI Express 4.0",
+      precio: 599.99,
+      categoria: 'graficas'
     },
+    // ... (otros componentes)
+  ],
+  discos: [
     {
-      modelo: "AMD Radeon RX 6700 XT",
-      memoria: "12 GB GDDR6",
-      busMemoria: "192 bits",
-      frecuenciaBase: "2321 MHz",
-      frecuenciaBoost: "2581 MHz",
-      consumoEnergia: "230 W",
-      tipoConector: "PCI Express 4.0",
-    },
-    {
-      modelo: "NVIDIA GeForce RTX 3060",
-      memoria: "12 GB GDDR6",
-      busMemoria: "192 bits",
-      frecuenciaBase: "1320 MHz",
-      frecuenciaBoost: "1777 MHz",
-      consumoEnergia: "170 W",
-      tipoConector: "PCI Express 4.0",
-    },
-  ];
-
-export const discos = [
-    {
+      id: 401,
       modelo: "Samsung 980 PRO",
       capacidad: "1 TB",
       tipo: "NVMe SSD",
@@ -179,16 +165,22 @@ export const discos = [
       velocidadEscritura: "5000 MB/s",
       factorForma: "M.2 2280",
       interfaz: "PCI Express 4.0",
+      precio: 199.99,
+      categoria: 'disco'
     },
     {
+      id: 402,
       modelo: "Seagate BarraCuda",
       capacidad: "2 TB",
       tipo: "HDD",
       velocidad: "7200 RPM",
       factorForma: "3.5 pulgadas",
       interfaz: "SATA 6 Gb/s",
+      precio: 79.99,
+      categoria: 'disco'
     },
     {
+      id: 403,
       modelo: "Western Digital Blue",
       capacidad: "500 GB",
       tipo: "SSD",
@@ -196,66 +188,45 @@ export const discos = [
       velocidadEscritura: "530 MB/s",
       factorForma: "2.5 pulgadas",
       interfaz: "SATA 6 Gb/s",
+      precio: 69.99,
+      categoria: 'disco'
     },
+    // ... (otros componentes)
+  ],
+  fuentes: [
     {
-      modelo: "ADATA XPG SX8200 Pro",
-      capacidad: "512 GB",
-      tipo: "NVMe SSD",
-      velocidadLectura: "3500 MB/s",
-      velocidadEscritura: "3000 MB/s",
-      factorForma: "M.2 2280",
-      interfaz: "PCI Express 3.0",
-    },
-    {
-      modelo: "Crucial MX500",
-      capacidad: "1 TB",
-      tipo: "SSD",
-      velocidadLectura: "560 MB/s",
-      velocidadEscritura: "510 MB/s",
-      factorForma: "2.5 pulgadas",
-      interfaz: "SATA 6 Gb/s",
-    },
-  ];
-
-export const fuentes = [
-    {
+      id: 501,
       modelo: "EVGA Supernova 850 T2",
       capacidad: "850 W",
       certificacion: "80 Plus Titanium",
       modular: true,
       eficiencia: "94%",
       ventilador: "135 mm",
+      precio: 219.99,
+      categoria: 'fuente'
     },
     {
+      id: 502,
       modelo: "Corsair RM750x",
       capacidad: "750 W",
       certificacion: "80 Plus Gold",
       modular: true,
       eficiencia: "90%",
       ventilador: "135 mm",
+      precio: 139.99,
+      categoria: 'fuente'
     },
     {
+      id: 503,
       modelo: "Seasonic Focus GX-650",
       capacidad: "650 W",
       certificacion: "80 Plus Gold",
       modular: true,
       eficiencia: "90%",
       ventilador: "120 mm",
+      precio: 99.99,
+      categoria: 'fuente'
     },
-    {
-      modelo: "Cooler Master MWE Gold 750 V2",
-      capacidad: "750 W",
-      certificacion: "80 Plus Gold",
-      modular: false,
-      eficiencia: "90%",
-      ventilador: "120 mm",
-    },
-    {
-      modelo: "Thermaltake Smart 700W",
-      capacidad: "700 W",
-      certificacion: "80 Plus",
-      modular: false,
-      eficiencia: "87%",
-      ventilador: "120 mm",
-    },
-  ];
+    // ... (otros componentes)
+  ],
+};
