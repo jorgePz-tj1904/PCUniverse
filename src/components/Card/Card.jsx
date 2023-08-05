9df8a81fe949dbd1faef15b70d9012beb379c71d
Rodrigo Alejandro Perez <rodrigo.alejandro.perez.104@gmail.com>
2023-08-04 22:35:01 -0300

    andaa

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Card = ({ id, especificaciones, img, modelo, precio, categoria, tipo })   => {
+  return (
+    <div className="card">
+
+    <Link to={`/componentes/${id}`}>
+      <img src={img} alt={modelo} />
+      <h3>{modelo}</h3>
+    </Link>
+      <p></p>
+
+      <p>Precio: ${precio}</p>
+      <p>{especificaciones}</p>
+      <p>{categoria}</p>
+     { tipo && tipo.length?(<p>{tipo}</p>): ""}
+
+    </div>
+  );
+};
+
+export default Card;
