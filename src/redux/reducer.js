6417f91939d4d66db226d9cb3315f381a284e065
Rodrigo Alejandro Perez <rodrigo.alejandro.perez.104@gmail.com>
2023-08-06 23:23:40 -0300

    paginado

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,38 +1,51 @@
-import { GET_COMPONENTS, GET_BY_NAME, POST_COMPONENTS, PAGINATE, DETAIL } from './actions-types'
+import {
+  GET_COMPONENTS,
+  GET_BY_NAME,
+  POST_COMPONENTS,
+  PAGINATE,
+  DETAIL,
+  FILTER,
+} from "./actions-types";
 
-const initailState = {
-    allComponents: [],
-    componentsP: [],
-    detail:[]
-}
+const initialState = {
+  allComponents: [],
+  componentsP: [],
+  detail: [],
+  filteredComponents:[]
+};
 
-function reducer(state = initailState, action) {
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_COMPONENTS:
+      return {
+        ...state,
+        componentsfilters: action.payload,
+        allComponents: action.payload,
+      };
 
-    switch (action.type) {
-        case GET_COMPONENTS:
-            return {
-                ...state,
-                componentsfilters: action.payload,
-                allComponents: action.payload,
-            };
+    case GET_BY_NAME:
+      return {
+        ...state,
+        allComponents: action.payload,
+        componentsP: [...action.payload],
+      };
 
-            //----------------------------Por Nombre---------------------------------------
-            case GET_BY_NAME:
-                return{
-                    ...state,
-                    allComponents:action.payload,
-                    componentsP: [...action.payload]
-                }
+    case DETAIL:
+      return {
+        ...state,
+        detail: action.payload,
+      };
+;
 
-                case DETAIL:
-                    return {
-                      ...state,
-                      detail: action.payload
-                    };
-            
-    
-        default: return state;
-    }
+    case FILTER:
+      return {
+        ...state,
+        filteredComponents: action.payload,
+      };
+
+    default:
+      return state;
+  }
 }
 
 export default reducer;
