354d0e12fbcfd1b7fcbbc6297ae4aed57684460c
Rodrigo Alejandro Perez <rodrigo.alejandro.perez.104@gmail.com>
2023-08-23 22:42:18 -0300

    home andando

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,24 @@
-import React, { useEffect } from "react";
-import Slider from "react-slick";
-import "./styles.css";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAllComponents } from '../../redux/actions';
+import Slider from 'react-slick';
+import './styles.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import foto1 from "../../assets/foto1.jpeg";
-import foto2 from "../../assets/foto2.jpeg";
-import foto3 from "../../assets/foto3.jpeg";
-import foto4 from "../../assets/foto4.jpeg";
-import foto5 from "../../assets/foto5.jpeg";
+import foto1 from "../../assets/foto1.jpeg"
+import foto2 from "../../assets/foto1.jpeg"
+import foto3 from "../../assets/foto1.jpeg"
+import foto4 from "../../assets/foto1.jpeg"
+import foto5 from "../../assets/foto1.jpeg"
+import FooterMain from '../Footer/FooterMain';
+import Card from "../../components/Card/Card";
+import axios from "axios"
 import { useAuth0 } from "@auth0/auth0-react";
-import axios from "axios";
 
 const Home = () => {
   const { user, isAuthenticated } = useAuth0();
+
+  console.log(user);
   useEffect(() => {
     if (isAuthenticated) {
       const userData = {
@@ -20,9 +26,7 @@ const Home = () => {
         name: user.given_name,
         last_name: user.family_name,
       };
-      if(user.email){
-        localStorage.setItem('usuario', JSON.stringify({email:user.email}));
-      }
+
       const sendUserDataToBackend = async () => {
         try {
           const response = await axios.post(
@@ -39,6 +43,8 @@ const Home = () => {
     }
   }, [isAuthenticated, user]);
 
+
+
   const settings = {
     dots: true,
     infinite: true,
@@ -54,85 +60,50 @@ const Home = () => {
     },
     autoplaySpeed: 3000,
   };
-  const Footer = () => {
-    const handleGoToTop = () => {
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    };
-  };
+  
+  const components = useSelector(state => state.allComponents);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllComponents());
+  }, [dispatch]);
+
+  // Filtrar solo los componentes de la categoría 'perifericos'
+  const selectedPeripherals = components.filter(component => component.categoria === 'periferico');
 
   return (
     <div>
       <div className="carousel-container">
         <Slider {...settings}>
-          {/* Imagen 1 */}
           <div>
             <img src={foto1} alt="Imagen 1" />
           </div>
-          {/* Imagen 2 */}
           <div>
             <img src={foto2} alt="Imagen 2" />
           </div>
-          {/* Imagen 3 */}
           <div>
-            <img src={foto3} alt="Imagen 2" />
+            <img src={foto3} alt="Imagen 3" />
           </div>
-          {/* Imagen 4 */}
           <div>
-            <img src={foto4} alt="Imagen 2" />
+            <img src={foto4} alt="Imagen 4" />
           </div>
-          {/* Imagen 5 */}
           <div>
-            <img src={foto5} alt="Imagen 2" />
+            <img src={foto5} alt="Imagen 5" />
           </div>
         </Slider>
       </div>
-
-      <div className="content">
-        <h2>ULTIMAS NOVEDADES</h2>
-        <hr className="divider" />
-        {/* Cards con las imágenes */}
-        <div className="cards-container">
-          {/* Card 1 */}
-          <div className="card">
-            <img
-              src="https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_36811_Silla_Gamer_Corsair_T3_Rush_Fabric_Carbon_88bef6b4-grn.jpg"
-              alt="Silla Gamer"
+      <div className="cards-container">
+        {selectedPeripherals.slice(0, 4).map(({ img, modelo, precio, categoria, id }) => (
+          <div className='cards-c' key={id}>
+            <Card
+              id={id}
+              img={img}
+              modelo={modelo}
+              precio={precio}
+              categoria={categoria}
             />
-            <h3>Silla Gamer Corsair T3 Rush Fabric Carbon</h3>
-            <p>Precio: $XXX</p>
           </div>
-          {/* Card 2 */}
-          <div className="card">
-            <img
-              src="https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_16752_Procesador_AMD_RYZEN_3_3200G_4.0GHz_Turbo___Radeon_Vega_8_AM4_Wraith_Stealth_Cooler_f414a507-grn.jpg"
-              alt="Procesador AMD Ryzen 3 3200G"
-            />
-            <h3>Procesador AMD Ryzen 3 3200G</h3>
-            <p>Precio: $XXX</p>
-          </div>
-          {/* Card 3 */}
-          <div className="card">
-            <img
-              src="https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_34993_Fuente_Be_Quiet_1000W_80_Plus_Gold_PURE_POWER_11_Full_Modular_3d25f718-grn.jpg"
-              alt="Fuente Be Quiet 1000W 80 Plus Gold PURE POWER 11 Full Modular"
-            />
-            <h3>
-              Fuente Be Quiet 1000W 80 Plus Gold PURE POWER 11 Full Modular
-            </h3>
-            <p>Precio: $XXX</p>
-          </div>
-          {/* Card 4 */}
-          <div className="card">
-            <img
-              src="https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_35714_Gabinete_Antec_NX292_MESH_RGB_Vidrio_Templado_a017a79f-grn.jpg"
-              alt="Gabinete Antec NX292 MESH RGB Vidrio Templado"
-            />
-            <h3>Gabinete Antec NX292 MESH RGB Vidrio Templado</h3>
-            <p>Precio: $XXX</p>
-          </div>
-        </div>
-
-        {/* Carrusel */}
+        ))}
       </div>
     </div>
   );
